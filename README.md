3486d8d2d8a460143c9657d6d68eb93272168ddc
Nathan Smith <nathan.smith.compsci@gmail.com>
2022-07-21 16:14:51 -0400

    Modified README file.

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 # Naive Raycaster
-This project was a very basic introduction for myself into rendering on a computer screen. This utilizes the very old method DOOM used. It sends out *n* amount of rays from the camera, and the distance they intersect with an object at, you draw a rectangle of width *delta d*, and height corresponding to the distance. This creates the illusion of a first person view.
+This utilizes the very old method Wolfenstein 3D used - albeit modified. It sends out *n* rays from the camera, and the distance they intersect with an object at, you draw a rectangle of width ![delta d](https://latex.codecogs.com/svg.image?\Delta&space;d) , and height corresponding to the distance. This creates the illusion of a first person view. The actual calculation method for determining intersection distance is different from the Wolfenstein 3D method - but the end result is visually identical. 
 
 ## Top-down view
-![A Gif showing the top-down view](top_down.gif)
+![A Gif showing the top-down view](docs/top_down.gif)
 
 You can change the view in the running program by pressing 'v'.
 
 ## First person view
-![A Gif showing the first person view](first_person.gif)
+![A Gif showing the first person view](docs/first_person.gif)
+*(This is not 1:1 with the above gif)*
+
+### More info
+This project was inspired by this [tutorial](https://lodev.org/cgtutor/raycasting.html).
--- raycaster.py
+++ raycaster.py
@@ -1,30 +1,46 @@
-from pygame import *
-from math import *
+import pygame as pg
+import math
 import random
 
-init()
+pg.init()
+
+# Global function declarations
+sin = math.sin
+cos = math.cos
+pi = math.pi
+sqrt = math.sqrt
 
 screen_width = 1600
 screen_height = 900
-screen = display.set_mode((screen_width,screen_height))
+screen = pg.display.set_mode((screen_width,screen_height))
+
+# ---------------------
+# Raycasting parameters
+# --------------------- 
+
 N = 200 # Number of rays
-Nw = 10 # Number of walls
-length = 2000 # Length of rays
+Nw = 10 # Number of walls generated
+length = 2000 # Length of rays (How far you can see)
 move_speed = 8
-look_speed = 7 # Rotation speed
-fov = 0.5*pi # In radians
+look_speed = 7
+fov = 0.5*pi # in radians
 x,y = 0,0 # Starting position
 raycaster_view = False # Default view, raycaster or first-person
-tick = 0
+view_change_cooldown = 15 # in ticks
 
-key.set_repeat(1)
+# ---------------------
+
+tick = 0
+pg.key.set_repeat(1)
+
 # Create random wall locations
 seg = [[(random.randint(0,screen_width),random.randint(0,screen_height)),(random.randint(0,screen_width),random.randint(0,screen_height))] for _ in range(Nw)]
 
 draw_width = screen_width//N
 view_angle_offset = 0
 drawing = True
-clock = time.Clock()
+clock = pg.time.Clock()
+
 
 def add_vec(p,q):
     return tuple(map(sum, zip(p,q)))
@@ -38,56 +54,60 @@ def dev_vec(p,q):
 def mul_vec(p,q):
     return tuple(map(lambda xy: xy[0]*xy[1],zip(p,q)))
 
-def cross_vec(p,q): # Defined 2D cross vector calculation
+def cross_vec(p,q): # Defined 2D vector cross calculation
     return p[0]*q[1]-p[1]*q[0]
 
 def dot_vec(p,q):
     return q[0]*p[0]+q[1]*p[1]
 
-# Main display loop
+
 while drawing:
-    clock.tick(30)
+    clock.tick(60)
     screen.fill((0,0,0))
-    keys = key.get_pressed()
-    event.get()
-    if tick < 100:
+    keys = pg.key.get_pressed()
+    pg.event.get()
+    if tick <= view_change_cooldown+1:
         tick += 1
     
-    if keys[K_w]: # Move forwards
+    if keys[pg.K_w]: # Move forward
         x += move_speed*cos((fov*(view_angle_offset+(N/2))/N))
         y += move_speed*sin((fov*(view_angle_offset+(N/2))/N))
-    elif keys[K_s]: # Move backward
+    elif keys[pg.K_s]: # Move backward
         x += move_speed*cos((fov*(view_angle_offset+(N/2))/N)+pi)
         y += move_speed*sin((fov*(view_angle_offset+(N/2))/N)+pi)
-    if keys[K_e]: # Strafe right
+    if keys[pg.K_e]: # Strafe right
         x += move_speed*cos((fov*(view_angle_offset+(N/2))/N)+pi/2)
         y += move_speed*sin((fov*(view_angle_offset+(N/2))/N)+pi/2)
-    elif keys[K_q]: # Strafe left
+    elif keys[pg.K_q]: # Strafe left
         x += move_speed*cos((fov*(view_angle_offset+(N/2))/N)+3*pi/2)
         y += move_speed*sin((fov*(view_angle_offset+(N/2))/N)+3*pi/2)
-    if keys[K_a]: # Look left
+    if keys[pg.K_a]: # Look left
         view_angle_offset -= look_speed
-    elif keys[K_d]: # Look right
+    elif keys[pg.K_d]: # Look right
         view_angle_offset += look_speed
-    if keys[K_v] and tick > 15: # Change view
+    if keys[pg.K_v] and tick > view_change_cooldown: # Change view
         tick = 0 # Reset cooldown
         raycaster_view = False if raycaster_view else True
-    if keys[K_ESCAPE]: # Exit
+    if keys[pg.K_ESCAPE]: # Exit
         drawing = False
-      
-    if raycaster_view:
-        for line in seg:
-            draw.line(screen, (100,100,100), line[0], line[1])
-    else:
-        draw.rect(screen, (90,90,90), (0,screen_height//2,screen_width,screen_height))
 
-    for ray in range(N):
+    if raycaster_view:
+        # Draw walls in 2D view
+        for line in seg:
+            pg.draw.line(screen, (100,100,100), line[0], line[1])
+    else:
+        # Draw floor in 3D view
+        pg.draw.rect(screen, (90,90,90), (0,screen_height//2,screen_width,screen_height))
+
+    # TODO: Reduce O(n²) runtime
+    for ray in range(N): # Check each ray for collision with wall
         dx = cos(fov*(ray+view_angle_offset)/N)
         dy = sin(fov*(ray+view_angle_offset)/N)
         x_0 = x+dx*length
         y_0 = y+dy*length
         rec_t = [] # Record t values from algorithm below
-        for wall in seg:
+
+        for wall in seg: # Check each wall for collision with current ray
             # Two vectors p+tr, and q+us
             # Implemented algorithm given from https://stackoverflow.com/questions/563198/how-do-you-detect-where-two-line-segments-intersect
             p = (x,y)
@@ -104,17 +124,19 @@ while drawing:
 
         height = 0
         colour = (0,0,0)
-        if rec_t:
+        if rec_t: # If the ray collides with one or more walls find the closest one
             angle = (fov*(view_angle_offset+(N/2))/N) - (fov*(ray+view_angle_offset)/N)
-            distance = min(rec_t)*abs(cos(angle)) # Obtaining true distance i.e. The shadow from the vector going to the wall, to the vector from where we view. This removes the fisheye effect
+            distance = min(rec_t)*abs(cos(angle)) # Obtaining true distance i.e. the shadow from the vector going to the wall, to the vector from where we view. This removes the fisheye effect
             height = (1/max(distance,0.1))*screen_height//10
             brightness = 1-sqrt(min(rec_t)) # Make brightness decay with the square of the distance
             colour = (brightness*255,brightness*255,brightness*255)
 
         if raycaster_view:
-            draw.line(screen, (255,255,255), (x,y), (x_0,y_0))
+            # Draw rays
+            pg.draw.line(screen, (255,255,255), (x,y), (x_0,y_0))
         else:
-            draw.rect(screen, colour, (draw_width*ray,screen_height//2-height//2,screen_width//N,height))
+            # Draw walls
+            pg.draw.rect(screen, colour, (draw_width*ray,screen_height//2-height//2,screen_width//N,height))
     
-    display.update() 
+    pg.display.update() 
 quit()
